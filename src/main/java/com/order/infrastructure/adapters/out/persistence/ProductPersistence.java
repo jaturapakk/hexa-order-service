440327462e0746a15eb8c8f3865a9fe57a97ac8c
jaturapakk <pak3535story@gmail.com>
2026-06-08 15:38:25 +0700

    feat: implement find product by id

--- src/main/java/com/order/infrastructure/adapters/out/persistence/ProductPersistence.java
+++ src/main/java/com/order/infrastructure/adapters/out/persistence/ProductPersistence.java
@@ -1,11 +1,15 @@
 package com.order.infrastructure.adapters.out.persistence;
 
 import com.order.application.ports.out.ProductRepository;
+import com.order.domain.model.Money;
 import com.order.domain.model.Product;
+import com.order.domain.model.ProductId;
+import com.order.domain.model.UserId;
 import com.order.infrastructure.adapters.out.persistence.entities.ProductEntity;
 import com.order.infrastructure.adapters.out.persistence.repository.JpaProductRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public class ProductPersistence implements ProductRepository {
 
@@ -26,4 +30,20 @@ public class ProductPersistence implements ProductRepository {
         ).toList();
         jpaProductRepository.saveAll(productEntities);
     }
+
+    @Override
+    public Optional<Product> findById(ProductId productId) {
+        return jpaProductRepository.findById(productId.value())
+                .map(this::mapToDomain);
+    }
+
+    private Product mapToDomain(ProductEntity productEntity){
+        return new Product(new ProductId(productEntity.getId()),
+                productEntity.getName(),
+                productEntity.getQuantity(),
+                new Money(productEntity.getPricePerUnit()),
+                new UserId(productEntity.getUserId())
+        );
+    }
+
 }
